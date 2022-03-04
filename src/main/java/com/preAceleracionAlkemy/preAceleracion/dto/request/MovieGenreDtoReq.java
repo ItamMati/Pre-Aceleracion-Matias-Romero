ca75459e0ac979d77ba0ff8b6ed76e570147825f
Matias <maticritam@gmail.com>
2022-03-04 02:37:25 -0600

    Desafio-3

--- src/main/java/com/preAceleracionAlkemy/preAceleracion/dto/request/MovieGenreDtoReq.java
+++ src/main/java/com/preAceleracionAlkemy/preAceleracion/dto/request/MovieGenreDtoReq.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
